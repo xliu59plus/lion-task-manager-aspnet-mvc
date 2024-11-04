693283b7fcd25315f1ecba34ecd6aa0c9217caf3
Runnan Li <rinali1028@gmail.com>
2024-11-03 19:29:44 -0800

    update keys

--- obj/Debug/net8.0/LionTaskManagementApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LionTaskManagementApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LionTaskManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d49d7b9779a6ded8c6d66caf19865e282af787af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+102dd04079a71745c5acfe5bf315af37edd9eca3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LionTaskManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LionTaskManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
